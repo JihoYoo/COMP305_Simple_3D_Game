90d5fbd4f1b7f172cc75a373fcb705911cb13f3f
JihoYoo <davidyoo214@gmail.com>
2016-03-17 23:47:14 -0400

    added map and a gun

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿/*
+ *  Jiho Yoo / 300813612
+ */
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//PUBLIC MEMBER VARIABLES
+	public Transform flashPoint;
+	public GameObject muzzleFlash;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Instantiate (this.muzzleFlash, flashPoint.position, Quaternion.identity);
+		}
+	}
+}
